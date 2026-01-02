6ed7d78f7c29ef150bf340f55c2a857a8e577cf5
Mehmet Fatih Duran <mefaran1@hotmail.com>
2026-01-03 00:53:03 +0300

    ui (arayüz)
    oyna butonu, ayarlar, çıkış ve ayar paneli yapıldı butonlar çalışıyor oyna butonuna basıldığında gelecek panel henüz eklenmedi

---
+++ tübitak1/ana_menu.gd
@@ -0,0 +1,27 @@
+extends Control
+
+# ayarlar panelini tanımlıyoruz
+@onready var ayarlar_paneli = $AyarlarPaneli
+
+func _ready():
+	# oyun açılınca panel kapalı olur
+	ayarlar_paneli.visible = false
+
+# oyna butonu
+func _on_oyna_butonu_pressed():
+	print("oyna butonu çalışıyor")
+
+# ayarlar butonu
+func _on_ayarlar_pressed():
+	# paneli görünür yaptık
+	ayarlar_paneli.visible = true
+
+# çıkış btonu
+func _on_cikis_pressed():
+	# oyunu kapatır
+	get_tree().quit()
+
+# paneldeki geri butonu
+func _on_geri_pressed(): 
+	# paneli gizle
+	ayarlar_paneli.visible = false
