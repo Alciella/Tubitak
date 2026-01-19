e8ac7378c1874459621ad22c89b60d5278355b85
Alciella <berkc2045@gmail.com>
2026-01-19 22:53:07 +0300

    bölüm 1 deprem

---
+++ tübitak1/yanlış.gd
@@ -0,0 +1,24 @@
+extends TextureRect
+
+var tween: Tween
+func açılıp_kaybol():
+	if tween:
+		tween.kill()
+	visible=true
+	modulate.a=1
+	tween=create_tween()
+	tween.tween_property(self,"modulate:a",1,0.1)
+	tween.tween_property(self,"modulate:a",0,0.5)
+	tween.finished.connect(func():
+		visible=false
+		)
+	
+	pass
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
